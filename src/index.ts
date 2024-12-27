d234010f65ce39a4f00d4296a76dd761d5d59ac8
Gera Maldonado <gmaldonadofelix@gmail.com>
2024-12-27 12:43:07 -0600

    change: refactorizando index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Usando TypeScript")
+import app from './app'
+
+const main = (): void => {
+  app.listen(app.get('port'), (): void => {
+    console.log(`Server on port: ${String(app.get('port'))}`)
+  })
+}
+
+main()
