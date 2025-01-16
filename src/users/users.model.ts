af88c6735bc7bc3cbcfbe0e7dc4a8d82ed7ba272
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-15 23:49:24 -0600

    add: logica para obtener los usarios y crear uno nuevo

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,9 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { CreateUserDTO, UserDTO } from './dtos/users.dto'
 
-const getAllUsers = (): Array<{ id: number, username: string }> => {
-  const result = [{ id: 1, username: 'user1' }, { id: 2, username: 'user2' }]
-  return (result)
+const prisma = new PrismaClient()
+
+const getAllUsers = async (): Promise<UserDTO[]> => {
+  const allUsers: UserDTO[] = await prisma.usuario.findMany({
+    select: {
+      id: true,
+      nombre: true,
+      nickname: true,
+      email: true,
+      fecha_creacion: true
+    }
+  })
+  return allUsers
+}
+
+const createUser = async (newUser: CreateUserDTO): Promise<string> => {
+  const id: string = randomUUID()
+  await prisma.usuario.create({ data: { id, ...newUser } })
+  return id
 }
 
 export default {
-  getAllUsers
+  getAllUsers,
+  createUser
 }
