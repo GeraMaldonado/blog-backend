4795e3d766bbc1e751fea0441f0c213ba7da2026
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-15 23:49:50 -0600

    add: logica para obtener los usarios y crear uno nuevo en controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express'
 import usersModel from './users.model'
+import { UserDTO } from './dtos/users.dto'
 
-interface User {
-  id: number
-  username: string
-}
-
-const getAllUsers = (_req: Request, res: Response): void => {
-  const users: User[] = usersModel.getAllUsers()
+const getAllUsers = async (_req: Request, res: Response): Promise<void> => {
+  const users: UserDTO[] = await usersModel.getAllUsers()
   res.json(users)
 }
 
+const createUser = async (req: Request, res: Response): Promise<void> => {
+  const { nombre, nickname, password, email } = req.body
+  const result = await usersModel.createUser({ nombre, nickname, password, email })
+  res.json({ result })
+}
+
 export default {
-  getAllUsers
+  getAllUsers,
+  createUser
 }
