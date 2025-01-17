f38a3a0918d103d6ce9ff1ad616fd8d9df465f99
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-16 21:13:58 -0600

    change: try catch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import usersModel from './users.model'
 import { UserDTO } from './dtos/users.dto'
+import { validateUser } from './users.validations'
+import { ValidationError } from '../errors/customizedError'
 
 const getAllUsers = async (_req: Request, res: Response): Promise<void> => {
   const users: UserDTO[] = await usersModel.getAllUsers()
@@ -8,9 +10,13 @@ const getAllUsers = async (_req: Request, res: Response): Promise<void> => {
 }
 
 const createUser = async (req: Request, res: Response): Promise<void> => {
-  const { nombre, nickname, password, email } = req.body
-  const result = await usersModel.createUser({ nombre, nickname, password, email })
-  res.json({ result })
+  try {
+    const { nombre, nickname, password, email } = validateUser(req.body)
+    const result = await usersModel.createUser({ nombre, nickname, password, email })
+    res.json({ result })
+  } catch (err: any) {
+    if (err instanceof ValidationError) res.status(500).json({ type: err.name, message: err.message })
+  }
 }
 
 export default {
