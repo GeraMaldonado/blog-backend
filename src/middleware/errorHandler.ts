aabd1a3a5cd8db0dd7d7a98629c96adb505b0c5d
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-21 16:21:54 -0600

    change: manejo de error para recurso no encontrado

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
-import { ValidationError } from '../errors/customizedError'
+import { NotFoundError, ValidationError } from '../errors/customizedError'
 
 export const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction): void => {
   if (err instanceof ValidationError) {
     res.status(400).json({ type: err.name, message: err.message })
+    return
+  }
+  if (err instanceof NotFoundError) {
+    res.status(404).json({ type: err.name, message: err.message })
     return
   }
 
