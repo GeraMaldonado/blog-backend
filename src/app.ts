08a2bd53f5a7c7154cc53e28dda6b2b84ebf1b89
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-23 15:45:36 -0600

    change: cambio de PORT

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { usersRouter } from './users/users.router'
 import { errorHandler } from './middleware/errorHandler'
+import { PORT } from './config'
 
 const app = express()
 
 app.use(express.json())
 
-app.set('port', 3000)
+app.set('port', PORT)
 
 app.use('/api', usersRouter)
 
