d76040c9b8bed468e53a64679352279de73d09e3
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-28 17:45:00 -0600

    change: quitando el console.log al modelo test

--- __tests__/users/users.model.ts
+++ __tests__/users/users.model.ts
@@ -3,37 +3,35 @@ import { CreateUserDTO, UpdateUserDTO, UserDTO } from '../../src/users/dtos/user
 import { IUserModel } from '../../src/users/IUserModel'
 import { ConflictError, NotFoundError } from '../../src/errors/customizedError'
 import { usersDatabase } from './users.database'
+import { encryptPassword } from '../../src/utils'
 
 export const UserModelTest: IUserModel = {
   async getAllUsers (): Promise<UserDTO[]> {
-    console.log(usersDatabase)
     return usersDatabase
   },
   async getUserById (id: string): Promise<UserDTO | null> {
     await this.validateUserExistance(id)
-    console.log(usersDatabase.find(u => u.id === id))
     return usersDatabase.find(u => u.id === id) ?? null
   },
   async createUser (newUser: CreateUserDTO): Promise<string> {
     await this.validateUniqueFields(newUser)
     const id = randomUUID()
+    newUser.password = await encryptPassword(newUser.password)
     usersDatabase.push({ id, fecha_creacion: new Date(), ...newUser })
-    console.log({ id, fecha_creacion: new Date(), ...newUser })
     return id
   },
   async updateUserById (id: string, updateUser: UpdateUserDTO): Promise<string> {
     await this.validateUserExistance(id)
     await this.validateUniqueFields(updateUser)
+    if (updateUser.password != null) updateUser.password = await encryptPassword(updateUser.password)
     const indexUser = usersDatabase.findIndex(u => u.id === id)
     usersDatabase[indexUser] = { ...usersDatabase[indexUser], ...updateUser }
-    console.log(updateUser)
     return `User ${id} updated`
   },
   async deleteUserById (id: string): Promise<string> {
     await this.validateUserExistance(id)
     const indexUser = usersDatabase.findIndex(u => u.id === id)
     usersDatabase.splice(indexUser, 1)
-    console.log(`user ${id} deleted`)
     return `User ${id} deleted`
   },
   async validateUserExistance (id: string): Promise<void> {
