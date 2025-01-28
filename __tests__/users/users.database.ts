30c502269850566d0c9f6fe17f3037abef5c4998
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-01-28 16:32:37 -0600

    change: cambio de password y correo

--- __tests__/users/users.database.ts
+++ __tests__/users/users.database.ts
@@ -1,21 +1,20 @@
 import { randomUUID } from 'node:crypto'
-import { IUser } from '../../src/users/dtos/users.dto'      
+import { IUser } from '../../src/users/dtos/users.dto'
 
 export const usersDatabase: IUser[] = [
   {
     id: randomUUID(),
     nombre: 'Max Rockatansky',
     nickname: 'MadMax',
-    email: 'gmaldonadofelix@gmail.com',
-    password: 'password123',
+    email: 'mrockatansky@email.com',
+    password: '$2a$12$TSG6/dDPUudI4v7QhDVDT.AeuV3UnboEfptoPz0mzanHIQYocR2fa',
     fecha_creacion: new Date()
   }, {
     id: randomUUID(),
     nombre: 'John Wick',
     nickname: 'Baba_Yaga',
-    email: 'gmaldonadofelix@gmail.com',
-    password: 'password123',
+    email: 'john_wick@email.com',
+    password: '$2a$12$U..P8k0DzyDhSMeMbQU8W.eMIKIQVQp9z2lm8mLjyl4ijxCuJATgK',
     fecha_creacion: new Date()
   }
 ]
-
