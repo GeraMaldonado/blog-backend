735fa61c19dd759f008aa91894a0c6a4564e6a8c
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-12 16:21:32 -0600

    change: mejorando la sintaxis

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -20,6 +20,7 @@ describe('User Endopints', () => {
       })
     })
   })
+
   describe('POST user', () => {
     it(`POST ${url} should create a user`, async () => {
       const newUser = { ...user }
@@ -27,6 +28,7 @@ describe('User Endopints', () => {
       expect(response.status).toBe(201)
       expect(typeof response.body.result).toBe('string')
     })
+
     it(`POST ${url} should fail for repeated user`, async () => {
       const newUser = { ...user, email: 'gmaldonadofelix@hotmail.com' }
       const response = await request(app).post(url).send(newUser)
@@ -34,6 +36,7 @@ describe('User Endopints', () => {
       expect(response.body.type).toEqual('ConflictError')
       expect(response.body.message).toEqual('nickname already is use')
     })
+
     it(`POST ${url} should fail for repeated email`, async () => {
       const newUser = { ...user, nickname: 'Owl' }
       const response = await request(app).post(url).send(newUser)
