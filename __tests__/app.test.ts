dcc901210d4af6db60645be752c11eb6d70ecf59
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-12 15:50:18 -0600

    add: test post en users con email duplicado

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -5,7 +5,7 @@ import { UserDTO } from '../src/users/dtos/users.dto'
 
 const app = createApp({ userModel: UserModelTest })
 const url: string = '/api/users'
-const newUser = { nombre: 'Gerardo Maldonado', password: 'passwordSeguro123', email: 'gmaldonadofelix@gmail.com', nickname: 'tHOwl953' }
+const user = { nombre: 'Gerardo Maldonado', password: 'passwordSeguro123', email: 'gmaldonadofelix@gmail.com', nickname: 'tHOwl953' }
 
 describe('User Endopints', () => {
   describe('GET All users', () => {
@@ -22,16 +22,24 @@ describe('User Endopints', () => {
   })
   describe('POST user', () => {
     it(`POST ${url} should create a user`, async () => {
+      const newUser = { ...user }
       const response = await request(app).post(url).send(newUser)
       expect(response.status).toBe(201)
       expect(typeof response.body.result).toBe('string')
     })
     it(`POST ${url} should fail for repeated user`, async () => {
-      newUser.email = 'gmaldonadofelix@hotmail.com'
+      const newUser = { ...user, email: 'gmaldonadofelix@hotmail.com' }
       const response = await request(app).post(url).send(newUser)
       expect(response.status).toBe(409)
       expect(response.body.type).toEqual('ConflictError')
       expect(response.body.message).toEqual('nickname already is use')
+    })
+    it(`POST ${url} should fail for repeated email`, async () => {
+      const newUser = { ...user, nickname: 'Owl' }
+      const response = await request(app).post(url).send(newUser)
+      expect(response.status).toBe(409)
+      expect(response.body.type).toEqual('ConflictError')
+      expect(response.body.message).toEqual('email already in use')
     })
   })
 })
