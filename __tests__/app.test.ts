f1ee659b5caaa08274077021f31bd955bd2870eb
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-12 14:22:40 -0600

    change: test para obtener todos los usuarios

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,13 +1,21 @@
 import request from 'supertest'
 import { createApp } from '../src/app'
-import { UserModelTest } from './users/users.model.test'
+import { UserModelTest } from './users/users.model'
+import { UserDTO } from '../src/users/dtos/users.dto'
 
 const app = createApp({ userModel: UserModelTest })
 
-describe('User Endpoints', () => {
-  it('GET /api should return a list of users', async () => {
-    const response = await request(app).get('/api')
-    console.log(response.body)
-    expect(response.status).toBe(200)
+describe('User Endopints', () => {
+  describe('GET All users', () => {
+    it('GET /api/users should return a list of users', async () => {
+      const response = await request(app).get('/api/users')
+      expect(response.status).toBe(200)
+      expect(Array.isArray(response.body)).toBe(true)
+      response.body.forEach((user: UserDTO) => {
+        expect(user).toHaveProperty('nickname')
+        expect(user).not.toHaveProperty('password')
+        expect(user).toHaveProperty('email')
+      })
+    })
   })
 })
