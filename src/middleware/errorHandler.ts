ed92edac04cffbd8ea14b918811ce8f688f86693
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-12 19:01:14 -0600

    change: comentando el console.error

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { ConflictError, NotFoundError, ValidationError } from '../errors/customizedError'
 
 export const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction): void => {
-  console.error(err)
+//  console.error(err.name, ': ', err.message)
   if (err instanceof ValidationError) {
     res.status(400).json({ type: err.name, message: err.message })
     return
