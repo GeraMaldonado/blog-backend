2585ddfbc81fee3e635b6d09d12877b5303b3110
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-15 23:32:45 -0600

    add: manejo del error sin autorizacion

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import { ConflictError, NotFoundError, ValidationError } from '../errors/customizedError'
+import { ConflictError, NotFoundError, UnauthorizedError, ValidationError } from '../errors/customizedError'
 
 export const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction): void => {
 //  console.error(err.name, ': ', err.message)
@@ -14,6 +14,9 @@ export const errorHandler = (err: any, _req: Request, res: Response, _next: Next
   if (err instanceof ConflictError) {
     res.status(409).json({ type: err.name, message: err.message })
     return
+  }
+  if (err instanceof UnauthorizedError) {
+    res.status(401).json({ type: err.name, message: err.message })
   }
 
   res.status(500).json({ type: 'InternalServerError', message: 'Something went wrong' })
