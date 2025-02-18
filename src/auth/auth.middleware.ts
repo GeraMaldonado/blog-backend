8c83ed4a64a96571360ba955feae855a08cc1c55
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-17 20:13:34 -0600

    add: implementando users en el request

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,17 +1,27 @@
 import { Request, Response, NextFunction } from 'express'
 import { verifyToken } from './auth.service'
 import { UnauthorizedError } from '../errors/customizedError'
+import { IUser } from '../interfaces/users/IUser'
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: IUser
+  }
+}
 
 export const authMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies?.accessToken
+    const token = req.cookies?.access_token
 
-    if (!token) throw new UnauthorizedError('Token not provided')
+    if (!token) {
+      return next(new UnauthorizedError('Token not provided'))
+    }
 
     const decoded = await verifyToken(token)
     req.user = decoded
+
     return next()
   } catch (err) {
-    next(new UnauthorizedError('Invalid token'))
+    return next(new UnauthorizedError('Invalid token'))
   }
 }
