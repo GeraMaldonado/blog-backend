c4b7d6915e0b96ebb26932ebc0e5ea476bcbb3af
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-17 20:17:46 -0600

    add: implementacion de middelwares token y auth en users

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import { UserController } from './users.controller'
 import { asyncHandler } from '../errors/asyncHandler'
 import { IUserModel } from '../interfaces/users/IUserModel'
+import { authMiddleware } from '../auth/auth.middleware'
+import { protectRoutes } from '../auth/protected.middleware'
 
 export const createUserRouter = ({ userModel }: { userModel: IUserModel }): Router => {
   const usersRouter = Router()
@@ -11,7 +13,7 @@ export const createUserRouter = ({ userModel }: { userModel: IUserModel }): Rout
   usersRouter.get('/', userController.getAllUsers)
   usersRouter.post('/', asyncHandler(userController.createUser))
   usersRouter.get('/:id', asyncHandler(userController.getUserById))
-  usersRouter.patch('/:id', asyncHandler(userController.updateUserById))
-  usersRouter.delete('/:id', asyncHandler(userController.deleteUserById))
+  usersRouter.patch('/:id', authMiddleware, protectRoutes, asyncHandler(userController.updateUserById))
+  usersRouter.delete('/:id', authMiddleware, protectRoutes, asyncHandler(userController.deleteUserById))
   return usersRouter
 }
