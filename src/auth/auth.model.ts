bc4477dab3e5cbcc5a98c833fe4826365a97c7e4
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-17 20:14:25 -0600

    add: creacion de modelo para confirmar usuarios

---
+++ src/auth/auth.model.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+import { encryptCompare } from '../utils'
+import { NotFoundError, UnauthorizedError } from '../errors/customizedError'
+import { AuthDTO, UserAuthDTO } from './dtos/auth.dto'
+const prisma = new PrismaClient()
+
+export class AuthModel {
+  static async authenticateUser ({ email, password }: AuthDTO): Promise<UserAuthDTO> {
+    const user = await prisma.user.findUnique({ where: { email } })
+    if (user == null) throw new NotFoundError('Invalid email or password')
+    const isPasswordValid = await encryptCompare(user.password, password)
+    if (!isPasswordValid) throw new UnauthorizedError('Invalid email or password')
+    return { id: user.id, username: user.username, email: user.email }
+  }
+}
