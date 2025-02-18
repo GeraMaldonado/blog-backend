ec6044931d44885c5004964fa83c927222a4f653
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-17 20:15:18 -0600

    add: nueva funcion para refrescar tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,30 @@
 import jwt, { Secret } from 'jsonwebtoken'
 import { SECRET_JWT_KEY, SECRET_REFRESH_JWT_KEY } from '../config'
+import { UserAuthDTO } from './dtos/auth.dto'
 
-export const generateAccessToken = async (user: any): Promise<string> => {
+export const generateAccessToken = async (user: UserAuthDTO): Promise<string> => {
   return jwt.sign(
-    { id: user.id, username: user.username },
+    { id: user.id, email: user.email, username: user.username },
     SECRET_JWT_KEY as Secret,
     { expiresIn: '15m' }
   )
 }
 
-export const generateRefreshToken = async (user: any): Promise<string> => {
+export const generateRefreshToken = async (user: UserAuthDTO): Promise<string> => {
   return jwt.sign(
     { id: user.id },
     SECRET_REFRESH_JWT_KEY as Secret,
     { expiresIn: '7d' }
   )
+}
+
+export const verifyRefreshToken = async (token: string): Promise<any> => {
+  return await new Promise((resolve, reject) => {
+    jwt.verify(token, SECRET_REFRESH_JWT_KEY as Secret, (err, decoded) => {
+      if (err != null) return reject(err)
+      resolve(decoded)
+    })
+  })
 }
 
 export const verifyToken = async (token: string): Promise<any> => {
