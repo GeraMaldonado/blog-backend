6490dfd2b0c3545568cf7830fd21c8428d079dad
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-02-19 18:38:16 -0600

    fix: arreglando que no sefimalizaba el codigo al generar el error

--- src/auth/protected.middleware.ts
+++ src/auth/protected.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { ForbiddenError } from '../errors/customizedError'
 
-export const protectRoutes = async (req: Request, _res: Response, next: NextFunction) => {
-  if (req.user?.id !== req.params.id) throw new ForbiddenError('You do not have permission to modify this resource')
+export const protectRoutes = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+  if (req.user?.id !== req.params.id) next(new ForbiddenError('You do not have permission to modify this resource'))
   next()
 }
