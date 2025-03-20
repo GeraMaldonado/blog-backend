d5721ef9e3c642057a96f7a4867c586babfff302
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-03-20 15:26:00 -0600

    add: valor por default a las constantes

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ if (process.env.NODE_ENV !== 'production') config()
 export const {
   PORT = 3000,
   SALT_ROUNDS = 10,
-  SECRET_JWT_KEY,
-  SECRET_REFRESH_JWT_KEY,
-  NODE_ENV,
-  DATABASE_URL,
-  DATABASE_URL_TEST
+  SECRET_JWT_KEY = 'SECRET_JWT_KEY',
+  SECRET_REFRESH_JWT_KEY = 'SECRET_REFRESH_JWT_KEY',
+  NODE_ENV = 'development',
+  DATABASE_URL = '',
+  DATABASE_URL_TEST = ''
 } = process.env
