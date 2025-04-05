469430187108c98c9679c1ebd7b3d83ebb04725c
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-05 17:07:05 -0600

    add: validaciones para los codigos y se agrego codigo a la de usuario

--- src/users/users.validations.ts
+++ src/users/users.validations.ts
@@ -5,18 +5,29 @@ const User = z.object({
   name: z.string().min(1, 'name is required').max(150, 'name is too long'),
   username: z.string().min(1, 'username is required').max(50, 'username is too long'),
   password: z.string().min(10, 'password must be al least 10 characters'),
-  email: z.string().email('invalid email format')
+  email: z.string().email('invalid email format'),
+  code: z.string().min(1, 'verification code is required')
 })
 
 export type ValidateUser = z.infer<typeof User>
 export type PartialValidateUser = z.infer<ReturnType<typeof User.partial>>
 
-const validateShcema = <T>(schema: z.ZodSchema<T>, input: unknown): T => {
+const validateSchema = <T>(schema: z.ZodSchema<T>, input: unknown): T => {
   const result = schema.safeParse(input)
   if (!result.success) throw new ValidationError(`required: ${result.error.errors.map(err => err.path).join(', ')}`)
   return result.data
 }
 
-export const validateUser = (input: unknown): ValidateUser => validateShcema(User, input)
+export const validateUser = (input: unknown): ValidateUser => validateSchema(User, input)
 
-export const validatePatialUser = (input: unknown): PartialValidateUser => validateShcema(User.partial(), input)
+export const validatePartialUser = (input: unknown): PartialValidateUser => validateSchema(User.partial(), input)
+
+export const VerificationRequest = z.object({
+  name: z.string().min(1, 'name is required').max(150, 'name is too long'),
+  email: z.string().email('invalid email format')
+})
+
+export type ValidateUserVerificationRequest = z.infer<typeof VerificationRequest>
+
+export const validateUserVerificationRequest = (input: unknown): ValidateUserVerificationRequest =>
+  validateSchema(VerificationRequest, input)
