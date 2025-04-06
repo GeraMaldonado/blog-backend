f22bfb04f8f1b167a5ab95ed312043c69d5dd0eb
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-05 18:33:36 -0600

    add: comprobacion tipo de entorno

--- src/services/userVerification.service.ts
+++ src/services/userVerification.service.ts
@@ -1,7 +1,8 @@
-import { cache } from './cache.service'
+import { cache, deleteVerificationCache } from './cache.service'
 import { sendEmail } from '../emails/email.service'
 import { generateVerificationCode } from '../utils/generateCode'
 import { InvalidCodeError } from '../errors/customizedError'
+import { NODE_ENV } from '../config'
 
 interface VerificationData {
   email: string
@@ -12,16 +13,19 @@ export const requestVerificationCode = async ({ name, email }: { name: string, e
   const code = generateVerificationCode()
   cache.set(email, { name, code })
 
-  await sendEmail(
-    email,
-    'Código de verificación',
-    `<p>Hola ${name}, tu codigo de verificacioón es <strong>${code}</strong></p>`
-  )
+  if (NODE_ENV !== 'test') {
+    await sendEmail(
+      email,
+      'Código de verificación',
+      `<p>Hola ${name}, tu codigo de verificacioón es <strong>${code}</strong></p>`
+    )
+  }
   return code
 }
 
 export const verifyCodeAndGetData = ({ email, code }: { email: string, code: string }): VerificationData => {
   const data = cache.get<VerificationData>(email)
   if ((data == null) || data.code !== code) throw new InvalidCodeError('invalid or expired verification code')
+  deleteVerificationCache(email)
   return data
 }
