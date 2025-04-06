c1f144cccef7997e65fdf18eb2731641b55d86f8
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-05 18:33:26 -0600

    add: comprobacion tipo de entorno

--- src/utils/generateCode.ts
+++ src/utils/generateCode.ts
@@ -1,3 +1,5 @@
+import { NODE_ENV } from '../config'
+
 export function generateVerificationCode (): string {
-  return Math.random().toString(36).substring(2, 8).toUpperCase()
+  return NODE_ENV === 'test' ? '123456' : Math.random().toString(36).substring(2, 8).toUpperCase()
 }
