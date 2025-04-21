e78b43a76c2b55dd946d10796fee00c61554973e
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-20 19:15:06 -0600

    add: interface para el modelo post

---
+++ src/interfaces/posts/IPostModel.ts
@@ -0,0 +1,9 @@
+import { CreatePostDTO, PostDTO, UpdatePostDTO } from '@/posts/dto/posts.dto'
+
+export interface IPostModel {
+  getAllPost: (userid: string) => Promise<PostDTO[]>
+  getPostById: (id: string) => Promise<PostDTO | null>
+  createPost: (newPost: CreatePostDTO) => Promise<string>
+  updatePostById: (id: string, updateUser: UpdatePostDTO) => Promise<string>
+  deletePostById: (id: string) => Promise<string>
+}
