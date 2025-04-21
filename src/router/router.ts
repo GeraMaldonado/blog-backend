00e8562bff25ab39c2f1a898f29054b7f278ccb4
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-20 19:17:55 -0600

    add: ruta post en ruta general

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,11 +3,14 @@ import { createUserRouter } from '../users/users.router'
 import { createAuthRouter } from '../auth/auth.router'
 import { IUserModel } from '../interfaces/users/IUserModel'
 import { IAuthModel } from '../interfaces/auth/IAuthModel'
+import { createPostsRouter } from '../posts/posts.router'
+import { IPostModel } from '@/interfaces/posts/IPostModel'
 
-export function createRouter ({ userModel, authModel }: { userModel: IUserModel, authModel: IAuthModel }): Router {
+export function createRouter ({ userModel, authModel, postModel }: { userModel: IUserModel, authModel: IAuthModel, postModel: IPostModel }): Router {
   const router = Router()
   router.use('/users', createUserRouter({ userModel }))
   router.use('/auth', createAuthRouter({ authModel }))
+  router.use('/posts', createPostsRouter({ postModel }))
 
   return router
 }
