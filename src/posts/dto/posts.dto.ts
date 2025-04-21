fb6f3ed0dd0f8fdc28c5268b10202d0f72e9ab8e
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-20 19:14:06 -0600

    add: dtos para crear, mostrar y modificar posts

---
+++ src/posts/dto/posts.dto.ts
@@ -0,0 +1,5 @@
+import { IPost } from '@/interfaces/posts/IPost'
+
+export type PostDTO = IPost
+export type CreatePostDTO = Pick<PostDTO, 'title' | 'content' | 'categoryId' | 'authorId'>
+export type UpdatePostDTO = Partial<CreatePostDTO>
