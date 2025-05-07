091081506b867fd14190abf1bb23364c72e49e90
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-07 16:20:26 -0600

    add: nueva ruta categorias

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,13 +4,15 @@ import { createAuthRouter } from '../auth/auth.router'
 import { IUserModel } from '../interfaces/users/IUserModel'
 import { IAuthModel } from '../interfaces/auth/IAuthModel'
 import { createPostsRouter } from '../posts/posts.router'
-import { IPostModel } from '@/interfaces/posts/IPostModel'
+import { IPostModel } from '../interfaces/posts/IPostModel'
+import { createCategoriesRouter } from '../categories/categories.router'
 
 export function createRouter ({ userModel, authModel, postModel }: { userModel: IUserModel, authModel: IAuthModel, postModel: IPostModel }): Router {
   const router = Router()
   router.use('/users', createUserRouter({ userModel }))
   router.use('/auth', createAuthRouter({ authModel }))
   router.use('/posts', createPostsRouter({ postModel }))
+  router.use('/categories', createCategoriesRouter())
 
   return router
 }
