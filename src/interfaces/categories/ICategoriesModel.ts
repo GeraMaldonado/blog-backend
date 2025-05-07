d607dfdeb8255994a64dcfa2616e981940c9f117
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-07 16:20:52 -0600

    add: interface para categorias y modelo cateogira

---
+++ src/interfaces/categories/ICategoriesModel.ts
@@ -0,0 +1,6 @@
+import { CategoryDTO, newCategoryDTO } from '@/categories/dto/categories.dto'
+
+export interface ICategoriesModel {
+  getAllCategories: (name: string) => Promise<CategoryDTO[]>
+  createCategory: (newCategory: newCategoryDTO) => Promise<string>
+}
