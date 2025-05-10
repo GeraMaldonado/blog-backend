701e26165a9080035cc1bd0330d532a83aa43c7d
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-10 13:12:09 -0600

    fix: retorno termprano para finalizar el middelware

--- src/auth/protected.middleware.ts
+++ src/auth/protected.middleware.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from 'express'
 import { ForbiddenError } from '../errors/customizedError'
 
 export const protectRoutes = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
-  if (req.user?.id !== req.params.id) next(new ForbiddenError('You do not have permission to modify this resource'))
+  if (req.user?.id !== req.params.id) return next(new ForbiddenError('You do not have permission to modify this resource'))
   next()
 }
