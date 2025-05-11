52f00a57236658bf64b52b1479bb4bb1722b9276
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-11 00:49:29 -0600

    add: proteccion a rutas para posts

--- src/auth/protected.middleware.ts
+++ src/auth/protected.middleware.ts
@@ -1,7 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
-import { ForbiddenError } from '../errors/customizedError'
+import { ForbiddenError, NotFoundError } from '../errors/customizedError'
+import { prisma } from '../database/mysql'
 
 export const protectRoutes = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
   if (req.user?.id !== req.params.id) return next(new ForbiddenError('You do not have permission to modify this resource'))
+  next()
+}
+
+export const protectPostOwner = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+  const post = await prisma.post.findUnique({ where: { id: req.params.id } })
+  if (!post) return next(new NotFoundError('post not found'))
+  if (post.authorId !== req.user?.id) return next(new ForbiddenError('You do not have permission to modify this post'))
+
   next()
 }
