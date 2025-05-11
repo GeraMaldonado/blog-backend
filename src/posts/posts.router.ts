d825c2532f380d19137e763b88c0012c32738d8f
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-11 00:51:00 -0600

    fix: protector correcto de ruta post

--- src/posts/posts.router.ts
+++ src/posts/posts.router.ts
@@ -3,7 +3,7 @@ import { PostsController } from './posts.controller'
 import { IPostModel } from '../interfaces/posts/IPostModel'
 import { asyncHandler } from '../errors/asyncHandler'
 import { authMiddleware } from '../auth/auth.middleware'
-import { protectRoutes } from '../auth/protected.middleware'
+import { protectPostOwner } from '../auth/protected.middleware'
 
 export function createPostsRouter ({ postModel }: { postModel: IPostModel }): Router {
   const postsRouter = Router()
@@ -12,9 +12,9 @@ export function createPostsRouter ({ postModel }: { postModel: IPostModel }): Ro
 
   postsRouter.get('/', asyncHandler(postsController.getAllPosts))
   postsRouter.get('/:id', asyncHandler(postsController.getPostById))
-  postsRouter.post('/', authMiddleware, protectRoutes, asyncHandler(postsController.createPost))
-  postsRouter.patch('/:id', authMiddleware, protectRoutes, asyncHandler(postsController.updatePostById))
-  postsRouter.delete('/:id', authMiddleware, protectRoutes, asyncHandler(postsController.deletePostById))
+  postsRouter.post('/', authMiddleware, asyncHandler(postsController.createPost))
+  postsRouter.patch('/:id', authMiddleware, protectPostOwner, asyncHandler(postsController.updatePostById))
+  postsRouter.delete('/:id', authMiddleware, protectPostOwner, asyncHandler(postsController.deletePostById))
 
   return postsRouter
 }
