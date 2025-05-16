f3a4b496d548b686e948f788f7198f93b14d4040
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-05-15 22:56:47 -0600

    add: nuevo tipo

--- src/posts/dto/posts.dto.ts
+++ src/posts/dto/posts.dto.ts
@@ -3,3 +3,23 @@ import { IPost } from '../../interfaces/posts/IPost'
 export type PostDTO = IPost
 export type CreatePostDTO = Pick<PostDTO, 'title' | 'content' | 'categoryId' | 'authorId'>
 export type UpdatePostDTO = Partial<CreatePostDTO>
+
+export type PopulatedPostDTO = {
+  id: string
+  title: string
+  content: string | null
+  publishedAt: Date
+  author: {
+    id: string
+    name: string
+    username: string
+  }
+  category: {
+    id: string
+    name: string
+  } | null
+  tags: {
+    id: string
+    name: string
+  }[]
+}
